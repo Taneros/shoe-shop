9ccd1a7a4a0a8f42344a41a0653812a0eccad62f
taneros <taneros@duck.com>
2024-01-05 17:20:03 +0300

    feat: add sear and filter panel

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
 import CardList from './components/CardList.vue';
 import Drawer from './components/Drawer.vue';
 import Header from './components/Header.vue'
+import SearchFilter from './components/SearchFilter.vue';
+
+
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
 
   <div class="w-4/5 mx-auto bg-white min-h-full rounded-xl shadow-xl">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <SearchFilter />
 
       <CardList />
+
+
     </div>
   </div>
 </template>
---
+++ src/components/SearchFilter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-between items-center mb-8">
+    <h2 class="text-3xl font-bold ">Все кроссовки</h2>
+
+    <div class="flex items-center gap-4">
+      <select class="py-2 px-3 border rounded-md outline-none">
+        <option>По названию</option>
+        <option>По цене (дорогие)</option>
+        <option>По цене (дешевые)</option>
+      </select>
+
+      <div class="relative">
+        <img src="/search.svg" alt="Search"
+             class="absolute top-1/2 left-3 transform -translate-y-1/2">
+        <input class=" border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400 transition" type="text"
+               placeholder="Поиск...">
+      </div>
+    </div>
+  </div>
+</template>
